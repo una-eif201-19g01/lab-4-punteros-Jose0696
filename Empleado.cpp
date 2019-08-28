c648cb7d32dff3631e64eb3f12ed69cc7d12ea60
José Miguel Sequeira Hernández <jose_seque@hotmail.com>
2019-08-28 11:05:46 -0600

    metodos cpp y h

--- Empleado.cpp
+++ Empleado.cpp
@@ -15,7 +15,69 @@
 
 #include"Empleado.h"
 
+Empleado::Empleado()
+{
+}
 
+Empleado::Empleado(const string & nombre, int anioExperiencia, float salarioBase)
+{
+}
 
+string Empleado::reporteEmpleado()
+{
+	return string();
+}
 
+string Empleado::getNombre()
+{
+	return string();
+}
 
+int Empleado::getNumeroEmpleado()
+{
+	return 0;
+}
+
+int Empleado::getAnioExperiencia() 
+{
+	return 0;
+}
+
+float Empleado::getSalarioBase()
+{
+	return 0.0f;
+}
+
+bool Empleado::getFlagRevision()
+{
+	return false;
+}
+
+void Empleado::setNombre(string)
+{
+}
+
+void Empleado::setNumeroEmpleado(int)
+{
+}
+
+void Empleado::setAnioExperiencia(int)
+{
+}
+
+void Empleado::setSalarioBase(float)
+{
+}
+
+void Empleado::setFlagRevision(bool)
+{
+}
+
+int Empleado::EmpleaaumentaSalario(const int *)
+{
+	return 0;
+}
+
+void Empleado::procesoRevision(bool *)
+{
+}
